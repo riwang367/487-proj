4ad988e083c73b4dfee3aa153e0fe5824a3bef1b
riwang <riwang@umich.edu>
2023-12-04 15:46:08 -0500

    Changing re.sub strings

--- preprocess.py
+++ preprocess.py
@@ -4,42 +4,42 @@ def preprocess(test_str):
     # eyes = "[8:=;]"
     # nose = "['`\-]?"
 
-    x = re.sub("http(s?+)", "", test_str) # Get rid of http because data has it on its own
-    x = re.sub("(www)?+\w+(com)\S*", "<URL>", x)
-    x = re.sub("/", " / ", x) # Force splitting words appended with slashes (once we tokenized the URLs, of course)
-    x = re.sub("u/[A-Za-z0-9_-]+", "<USER>", x) #u/(username) format on Reddit
+    x = re.sub(r"http(s?+)", "", test_str) # Get rid of http because data has it on its own
+    x = re.sub(r"(www)?+\w+(com)\S*", "<URL>", x)
+    x = re.sub(r"/", " / ", x) # Force splitting words appended with slashes (once we tokenized the URLs, of course)
+    x = re.sub(r"u/[A-Za-z0-9_-]+", "<USER>", x) #u/(username) format on Reddit
     # :) smile: (8|:|=|;)('|`|\\|-)*)+
     # (: smile: \(+('|`|\\|-)*(8|:|=|;)
-    x = re.sub("((8|:|=|;)('|`|\\|-)*)+|\(+('|`|\\|-)*(8|:|=|;)", "<SMILE>", x)
-    x = re.sub("(8|:|=|;)('|`|\\|-)*p+", "<LOLFACE>", x)
+    x = re.sub(r"((8|:|=|;)('|`|\\|-)*)+|\(+('|`|\\|-)*(8|:|=|;)", "<SMILE>", x)
+    x = re.sub(r"(8|:|=|;)('|`|\\|-)*p+", "<LOLFACE>", x)
     # :( sad: (8|:|=|;)('|`|\\|-)*)*\(+
     # ): sad: ()+)('|`|\\|-)*)*(8|:|=|;))
     # (8|:|=|;)('|`|\\|-)*\(+
     # \(+('|`|\\|-)*(8|:|=|;)
-    x = re.sub("((8|:|=|;)('|`|\\|-)*[(]+|()+('|`|\\|-)*(8|:|=|;))", "<SADFACE>", x)
-    x = re.sub("(8|:|=|;)('|`|\\|-)*(\/|l*)+", "<NEUTRALFACE>", x)
-    x = re.sub("<3","<HEART>", x)
-    x = re.sub("[-+]?[.\d]*[\d]+[:,.\d]*/", "<NUMBER>", x)
-    x = re.sub("([!?.]){2,}/", repeat_sub(x), x) # Mark punctuation repeats
-    x = re.sub("\b(\S*?)(.)\2{2,}\b", elongate_sub(x), x)
+    x = re.sub(r"((8|:|=|;)('|`|\\|-)*[(]+|()+('|`|\\|-)*(8|:|=|;))", "<SADFACE>", x)
+    x = re.sub(r"(8|:|=|;)('|`|\\|-)*(\/|l*)+", "<NEUTRALFACE>", x)
+    x = re.sub(r"<3","<HEART>", x)
+    x = re.sub(r"[-+]?[.\d]*[\d]+[:,.\d]*/", "<NUMBER>", x)
+    x = re.sub(r"([!?.]){2,}/", repeat_sub(x), x) # Mark punctuation repeats
+    x = re.sub(r"\b(\S*?)(.)\2{2,}\b", elongate_sub(x), x)
 
     return x
 
 
 def repeat_sub(str):
     # Mark punctuation repeats
-    repeat = re.findall("([!?.]){2,}", str)
+    repeat = re.findall(r"([!?.]){2,}", str)
     new_str = ""
     for r in repeat:
         new_str += r[0]
-    x = re.sub("([!?.]){2,}", new_str + " <REPEAT>", str) 
+    x = re.sub(r"([!?.]){2,}", new_str + " <REPEAT>", str) 
     return x
 
 
 def elongate_sub(str):
     # Mark elongated words
-    repeat = re.split("\b(\S*?)(.)\2{2,}\b", str)
-    x = re.sub("\b(\S*?)(.)\2{2,}\b", repeat[0] + " <ELONG>", str)
+    repeat = re.split(r"\b(\S*?)(.)\2{2,}\b", str)
+    x = re.sub(r"\b(\S*?)(.)\2{2,}\b", repeat[0] + " <ELONG>", str)
     return x
 
 # data has already been .lower()ed
